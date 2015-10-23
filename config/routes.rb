67c5cf194a58897397d2693f142eacc407f76e54
Yvonne Clarke <eveclarke91@gmail.com>
2015-10-23 10:40:20 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
           get 'about'   => 'static_pages#about'
           get 'contact' => 'static_pages#contact'
           get 'signup'  => 'users#new'
+          get '/signup' => 'users#new'
+
          
 
       end
